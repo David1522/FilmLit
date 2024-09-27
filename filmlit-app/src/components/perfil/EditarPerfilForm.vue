1a693c1321b8d002a28afa76ed1b54eca36a4db9
Unumvahk <parrasabadunum@gmail.com>
2024-09-27 14:20:31 -0500

    Componentes editar perfil y publicaciones añadidos

--- filmlit-app/src/components/perfil/EditarPerfilForm.vue
+++ filmlit-app/src/components/perfil/EditarPerfilForm.vue
@@ -1,37 +1,46 @@
-<template>
-    <div>
-        <h2 id="edit">Editar Perfil</h2>
-        <div class="profile-edit">
-        <form>
-
-        <div class="picture">
-            <label for="pic">Editar Foto:</label>
-            <input type="file" id="pic" name="pic">
-        </div>
-
-        <div class="info">
-            <label for="name">Nombre:</label>
-            <input type="text" id="name" name="name">
-            <br>
-            <label for="uname">Arroba:</label>
-            <input type="text" id="uname" name="uname">
-            <br>
-            <label for="desc">Descripción:</label>
-            <textarea id="desc" name="desc"></textarea>
-        </div>
-
-        <div class="actions">
-            <button type="submit">Guardar Cambios</button>
-            <button type="button" class="delete">Eliminar Perfil</button>
-        </div>
-        </form>
-    </div>
-    </div>
-</template>
-
-<script setup>
+<script>
 
 </script>
+<template>
+
+    <div>
+
+        <div class="perfil-contenedor">
+            <h2 id="editar-perfil">Editar Perfil</h2>
+            <form>
+                <div class="foto-contenedor">
+                    <label for="foto" class="foto-titulo">Editar Foto:</label>
+                    <div class="foto-imagen-caja">
+                        <img id="imagen" src="#" alt="" />
+                    </div>
+                    <input type="file" id="agregar-foto" name="foto" class="input-foto">
+                </div>
+
+                <div class="informacion-contenedor">
+                    <label for="nombre" class="label-nombre">Nombre</label>
+                    <input type="text" id="nombre" name="nombre" class="input-nombre">
+                    <br>
+                    <label for="nombre" class="label-nombre">Nombre de Usuario</label>
+                    <input type="text" id="nombre" name="nombre" class="input-nombre">
+                    <br>
+                    <label for="usuario" class="label-usuario">Arroba</label>
+                    <input type="text" id="usuario" name="usuario" class="input-usuario">
+                    <br>
+                    <label for="nombre" class="label-nombre">sexo</label>
+                    <input type="text" id="nombre" name="nombre" class="input-nombre">
+                    <br>
+                    <label for="descripcion" class="label-descripcion">Descripción</label>
+                    <textarea id="descripcion" name="descripcion" class="input-descripcion"></textarea>
+                </div>
+
+                <div class="acciones-contenedor">
+                    <button type="submit" class="btn-guardar">Guardar Cambios</button>
+                    <button type="button" class="btn-eliminar">Eliminar Perfil</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
 
 <style lang="scss" scoped>
     main {
@@ -40,75 +49,108 @@
         justify-content: center;
     }
 
-    label {
-        color: black
+    #editar-perfil {
+        color: rgb(255, 255, 255);
     }
 
-    .profile-edit {
-        background-color: #f4f4f4;
+    .perfil-contenedor {
+        margin-left:27%;
+        background-color: #3a2064;
         border-radius: 8px;
         padding: 20px;
         width: 100%;
+        height:785px;
+        margin-top:20px;
         max-width: 600px;
-        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        box-shadow: 0 4px 8px rgba(255, 255, 255, 0.726);
     }
 
-    .profile-edit h2 {
+    .foto-contenedor {
+        text-align: center;
         margin-bottom: 20px;
-        font-size: 1.5em;
     }
 
-    .profile-edit .picture, .profile-edit .info {
-        margin-bottom: 15px;
+    .foto-titulo {
+        font-size: 1.2em;
+        margin-bottom: 10px;
+        display: block;
+        font-weight: bold;
+        color: #fcfcfc;
     }
 
-    .profile-edit label {
+    .foto-imagen-caja {
+        width: 150px;
+        height: 150px;
+        margin: 0 auto;
+        border-radius: 50%;
+        border: 2px solid #ddd;
+        overflow: hidden;
+    }
+
+    .foto-imagen-caja img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    .input-foto {
+        display: block;
+        margin: 20px auto;
+        padding: 10px;
+        margin-left:200px;
+        background-color: #00000000;
+    }
+
+    .informacion-contenedor {
+        margin-bottom: 20px;
+    }
+
+    .label-nombre, .label-usuario, .label-descripcion {
         display: block;
         margin-bottom: 5px;
         font-weight: bold;
+        color:#f7f7f7;
+        border-color: #000000;
     }
 
-    .profile-edit input[type="text"],
-    .profile-edit input[type="file"],
-    .profile-edit textarea {
+    .input-nombre, .input-usuario, .input-descripcion {
         width: 100%;
         padding: 10px;
-        border: 1px solid #ddd;
+        border: 1px solid #dddddd6b;
         border-radius: 4px;
+        background-color: #0000003a;
     }
 
-    .profile-edit textarea {
+    .input-descripcion {
         resize: vertical;
         min-height: 100px;
     }
 
-    .profile-edit .actions {
+    .acciones-contenedor {
         display: flex;
         justify-content: space-between;
     }
 
-    .profile-edit .actions button {
+    .btn-guardar {
         padding: 10px 15px;
         border: none;
         border-radius: 4px;
         cursor: pointer;
+        background-color: #ffffff48;
+        color: rgb(255, 255, 255);
     }
 
-    .profile-edit .actions button[type="submit"] {
-        background-color: #3a2064;
-        color: white;
-    }
-
-    .profile-edit .actions .delete {
+    .btn-eliminar {
+        padding: 10px 15px;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
         background-color: #e74c3c;
         color: white;
     }
 
-    .profile-edit .actions button:hover {
+    .btn-guardar:hover, .btn-eliminar:hover {
         opacity: 0.9;
     }
-    #edit{
-        margin-top:20 px;
-        margin-right:20px;
-    }
+
 </style>
--- filmlit-app/src/components/publicaciones/EditarPublicacion.vue
+++ filmlit-app/src/components/publicaciones/EditarPublicacion.vue
@@ -1,28 +1,28 @@
+
 <template>
     <div>
-        
         <div class="publicacion-contenedor">
-            <h2 id="nueva-publicacion-titulo">Nueva Publicación</h2>
+            <h2 id="editar-publicacion">Editar Publicación</h2>
             <form>
-                <div class="publicacion-imagen-contenedor">
-                    <label for="publicacion-foto" class="publicacion-imagen-titulo"></label>
-                    <div class="publicacion-imagen-caja">
-                       
+                <div class="foto-contenedor">
+                    <label for="foto-publicacion" class="foto-titulo">Editar Imagen:</label>
+                    <div class="foto-imagen-caja">
+                        <img id="imagen-publicacion" src="#" alt="" />
                     </div>
-                    <input type="file" id="publicacion-foto" name="foto" class="input-publicacion-foto">
+                    <input type="file" id="foto-publicacion" name="foto" class="input-foto">
                 </div>
 
-                <div class="publicacion-info-contenedor">
-                    <label for="titulo" class="label-titulo">Título:</label>
-                    <input type="text" id="titulo" name="titulo" class="input-titulo">
+                <div class="informacion-contenedor">
+                    <label for="titulo-publicacion" class="label-titulo">Título</label>
+                    <input type="text" id="titulo-publicacion" name="titulo" class="input-titulo">
                     <br>
-                    <label for="descripcion-publicacion" class="label-descripcion-publicacion">Descripción:</label>
-                    <textarea id="descripcion-publicacion" name="descripcion-publicacion" class="input-descripcion-publicacion"></textarea>
+                    <label for="descripcion-publicacion" class="label-descripcion">Descripción</label>
+                    <textarea id="descripcion-publicacion" name="descripcion" class="input-descripcion"></textarea>
                 </div>
 
-                <div class="acciones-publicacion-contenedor">
-                    <button type="submit" class="btn-publicar">Publicar</button>
-                    <button type="reset" class="btn-cancelar">Cancelar</button>
+                <div class="acciones-contenedor">
+                    <button type="submit" class="btn-guardar">Guardar Cambios</button>
+                    <button type="button" class="btn-eliminar">Eliminar Publicación</button>
                 </div>
             </form>
         </div>
@@ -36,87 +36,87 @@
         justify-content: center;
     }
 
-    #nueva-publicacion-titulo {
-        color: rgb(255, 255, 255);
+    #editar-publicacion {
+        color: rgb(247, 247, 247);
     }
 
     .publicacion-contenedor {
+        margin-left: 27%;
         background-color: #3a2064;
         border-radius: 8px;
         padding: 20px;
         width: 100%;
+        height:600px;
+        margin-top: 40px;
         max-width: 600px;
-        margin-left:27%;
-        margin-top:20px;
         box-shadow: 0 4px 8px rgba(255, 255, 255, 0.726);
     }
 
-    .publicacion-imagen-contenedor {
+    .foto-contenedor {
         text-align: center;
         margin-bottom: 20px;
     }
 
-    .publicacion-imagen-titulo {
+    .foto-titulo {
         font-size: 1.2em;
         margin-bottom: 10px;
         display: block;
         font-weight: bold;
-        color: #000000;
+        color: #fffefe;
     }
-
-    .publicacion-imagen-caja {
+   
+    .foto-imagen-caja {
         width: 150px;
         height: 150px;
         margin: 0 auto;
-        border-radius: 10px;
+        border-radius: 50%;
         border: 2px solid #ddd;
         overflow: hidden;
     }
 
-    .publicacion-imagen-caja img {
+    .foto-imagen-caja img {
         width: 100%;
         height: 100%;
         object-fit: cover;
     }
 
-    .input-publicacion-foto {
+    .input-foto {
         display: block;
         margin: 20px auto;
         padding: 10px;
+        margin-left: 200px;
     }
 
-    .publicacion-info-contenedor {
+    .informacion-contenedor {
         margin-bottom: 20px;
     }
 
-    .label-titulo, .label-descripcion-publicacion {
+    .label-titulo, .label-descripcion {
         display: block;
         margin-bottom: 5px;
         font-weight: bold;
-        color:#ffffff;
-        border-color: #000000;
+        color: #ffffff;
     }
 
-    .input-titulo, .input-descripcion-publicacion {
+    .input-titulo, .input-descripcion {
         width: 100%;
         padding: 10px;
         border: 1px solid #dddddd6b;
         border-radius: 4px;
-        color:#ffffff;
         background-color: #0000003a;
     }
 
-    .input-descripcion-publicacion {
+    .input-descripcion {
         resize: vertical;
         min-height: 100px;
     }
 
-    .acciones-publicacion-contenedor {
+    .acciones-contenedor {
         display: flex;
         justify-content: space-between;
     }
 
-    .btn-publicar {
+    .btn-guardar {
         padding: 10px 15px;
         border: none;
         border-radius: 4px;
@@ -125,7 +125,7 @@
         color: white;
     }
 
-    .btn-cancelar {
+    .btn-eliminar {
         padding: 10px 15px;
         border: none;
         border-radius: 4px;
@@ -134,7 +134,7 @@
         color: white;
     }
 
-    .btn-publicar:hover, .btn-cancelar:hover {
+    .btn-guardar:hover, .btn-eliminar:hover {
         opacity: 0.9;
     }
 

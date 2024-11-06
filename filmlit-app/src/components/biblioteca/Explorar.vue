dc19d316b89ec808b1a1983c9f310fa8199297ad
sebas <sebastian.df1304@gmail.com>
2024-11-05 22:57:38 -0500

    Biblioteca añadida sin estilos ni funcionalidad de guardar

---
+++ filmlit-app/src/components/biblioteca/Explorar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="button-container">
+            <router-link :to = "{name: 'Peliculas'}">Peliculas</router-link>
+            <router-link :to = "{name: 'Series'}">Series</router-link>
+            <router-link :to = "{name: 'Libros'}">Libros</router-link>
+        </div>
+        <router-view /> <!-- Aquí se cargarán las vistas hijas -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Publicaciones'
+};
+</script>
+
+<style scoped>
+.button-container {
+    display: flex;
+    gap: 10px;
+    margin-bottom: 20px;
+}
+button {
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+}
+</style>
